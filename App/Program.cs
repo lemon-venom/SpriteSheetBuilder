1f718be80059a4b4f32e45b8d6dc3779b46d4e37
Michael <Michael@ZODIAC>
2023-05-04 01:10:20 -0400

    Initial Commit

---
+++ App/Program.cs
@@ -0,0 +1,31 @@
+﻿using SpriteSheetBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpriteSheetBuilderUtility
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            ISpriteSheetBuilderDialog spriteSheetBuilder = new SpriteSheetBuilderDialog();
+
+            Form applicationMainForm = ((Form)spriteSheetBuilder);
+
+            applicationMainForm.ShowInTaskbar = true;
+            
+            Application.Run(applicationMainForm);
+        }
+    }
+}
